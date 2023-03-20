2cf08cac4cdf89cc0f01d4b64f0547f75acfe2ff
emil584c <emilwaellind@gmail.com>
2023-03-20 14:44:59 +0100

    .

---
+++ vueundervisning/tutorials/Deeppink/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue';
+const vandcykel = ref('');
+
+function onSubmit (){
+  alert (vandcykel.value);
+}
+
+</script>
+
+<template>
+
+  <input type="text" v-model="vandcykel" />
+  <button v-on:click="onSubmit">
+    KANP LOL
+  </button>
+  {{ vandcykel }}
+  
+</template>
+
---
+++ vueundervisning/tutorials/opgave-3/src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+  import { ref } from 'vue';
+  let fornavn = ref('');
+  let efternavn = ref('');
+  let email = ref('');
+  let dato = ref('');
+  
+
+  function onSubmit (){
+
+  }
+
+</script>
+
+<template>
+  
+<div>
+  <input type="text" v-model="fornavn" />
+  <input type="text" v-model="efternavn"/>
+  <input type="text" v-model="email"/>
+  <input type="text" v-model="dato"/>
+</div>
+
+<div>
+  <h3>Navn = {{ fornavn + " " + efternavn }}</h3>
+  <p>email = {{ email }}</p>
+  <p>Fødselsdato = {{ dato }}</p>
+  <button v-on:click="onSubmit">Gem information</button>
+  <p>{{ info }}</p>
+</div>
+
+
+
+</template>
+
+<style scoped>  
+
+</style>
